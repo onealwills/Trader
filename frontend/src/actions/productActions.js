823b216a7774e26e4756e0d2581ec66564268294
williams <oneal.wills@gmail.com>
2022-01-03 08:47:49 +0100

    search box

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,13 +20,15 @@ import {
 } from "../constants/productConstants";
 
 export const listProducts =
-  ({ seller = "" }) =>
+  ({ seller = "", name = "" }) =>
   async (dispatch) => {
     dispatch({
       type: PRODUCT_LIST_REQUEST,
     });
     try {
-      const { data } = await axios.get(`/api/products?seller=${seller}`);
+      const { data } = await axios.get(
+        `/api/products?seller=${seller}&name=${name}`
+      );
       dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
     } catch (error) {
       dispatch({ type: PRODUCT_LIST_SUCCESS, payload: error.message });
