df7bffaa89000a5d5e5b9c8dc42b0715d763540d
williams <oneal.wills@gmail.com>
2023-02-20 17:38:43 +0100

    build npm

---
+++ frontend/build/precache-manifest.4eed17ffa3954848497400a351b1040e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673e8f729559d7b212b1d48f0f1a2d71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "214f0744f08f8acca17c",
+    "url": "/static/css/2.c7661d7b.chunk.css"
+  },
+  {
+    "revision": "06232567236d339c364d",
+    "url": "/static/css/main.7429e7ee.chunk.css"
+  },
+  {
+    "revision": "214f0744f08f8acca17c",
+    "url": "/static/js/2.9d961d6a.chunk.js"
+  },
+  {
+    "revision": "54bc0e688f3a6b451b674dc6589c247a",
+    "url": "/static/js/2.9d961d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06232567236d339c364d",
+    "url": "/static/js/main.8702cc2e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
